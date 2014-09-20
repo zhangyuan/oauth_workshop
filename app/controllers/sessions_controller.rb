610db9b1f75f14ea120950d7b9807abd6a5bc76d
Yuan Cheung <zhangyuan@users.noreply.github.com>
2014-09-20 15:44:25 +0800

    auth callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def callback
+    connection = Faraday.new
+    body = {client_id: Rails.application.secrets.app_key, 
+            client_secret: Rails.application.secrets.app_secret, 
+            code: params[:code], 
+            grant_type: "authorization_code", 
+            redirect_uri: Rails.application.secrets.callback_url 
+    }
+
+    response = connection.post do |req|
+      req.url "https://api.weibo.com/oauth2/access_token", body
+    end
+
+    access_token = MultiJson.decode(response.body)
+
+    session[:uid] = access_token['uid']
     redirect_to root_url 
   end
 end
--- spec/requests/sign_ins_spec.rb
+++ spec/requests/sign_ins_spec.rb
@@ -15,13 +15,31 @@ RSpec.describe "Sign In", :type => :request do
 
     describe "callback request" do
       before(:each) do
-        get "/auth/callback/weibo?code=3db321504fb7cae7aca7738009666a16"
+        json =  {
+          access_token: "ACCESS_TOKEN",
+          expires_in: 1234,
+          remind_in:"798114",
+          uid:"12341234"
+        }.to_json
+
+        body = {
+          client_id: Rails.application.secrets.app_key,
+          client_secret: Rails.application.secrets.app_secret,
+          code: "CODE",
+          grant_type: "authorization_code",
+          redirect_uri:  "http://ws.mytw.org:3000/auth/callback/weibo"
+        }
+
+        stub_request(:post, "https://api.weibo.com/oauth2/access_token?" + body.to_query).to_return(body: json)
+
+        get "/auth/callback/weibo?code=CODE"
       end
 
-      it "redircts to root page without sign in button" do
+      it "redircts to root page without sign in button and with uid" do
         expect(response).to redirect_to(root_url)
         follow_redirect!
         expect(response.body).not_to include("微博登录")
+        expect(response.body).to include("12341234")
       end
     end
   end
