938d42d4146dad0d41550ac0aeb2b76c8f22612a
Yuan Cheung <zhangyuan@users.noreply.github.com>
2014-09-21 15:49:23 +0800

    extract auth to class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,18 @@
 class SessionsController < ApplicationController
   def connect
-    url = "https://api.weibo.com/oauth2/authorize?client_id=#{Rails.application.secrets.app_key}&response_type=code&redirect_uri=#{CGI.escape Rails.application.secrets.callback_url}" 
-    redirect_to url
+    redirect_to auth_client.authorize_url(Rails.application.secrets.callback_url)
   end
 
   def callback
-    connection = Faraday.new
-    body = {client_id: Rails.application.secrets.app_key, 
-            client_secret: Rails.application.secrets.app_secret, 
-            code: params[:code], 
-            grant_type: "authorization_code", 
-            redirect_uri: Rails.application.secrets.callback_url 
-    }
-
-    response = connection.post do |req|
-      req.url "https://api.weibo.com/oauth2/access_token", body
-    end
-
-    access_token = MultiJson.decode(response.body)
-
+    access_token = auth_client.get_token(params[:code], Rails.application.secrets.callback_url)
     session[:uid] = access_token['uid']
     redirect_to root_url 
   end
+
+  private
+  def auth_client
+    Weibo::AuthClient.new(Rails.application.secrets.app_key, Rails.application.secrets.app_secret)
+  end
 end
+
+
---
+++ lib/weibo/auth_client.rb
@@ -0,0 +1,28 @@
+module Weibo
+  class AuthClient
+    def initialize(client_id, client_secret)
+      @client_id, @client_secret = client_id, client_secret 
+    end
+
+    def authorize_url(redirect_uri)
+      url = "https://api.weibo.com/oauth2/authorize?client_id=#{@client_id}&response_type=code&redirect_uri=#{CGI.escape redirect_uri}" 
+    end
+
+    def get_token(code, redirect_uri)
+      connection = Faraday.new
+      body = {
+        client_id: @client_id, 
+        client_secret: @client_secret, 
+        code: code, 
+        grant_type: "authorization_code", 
+        redirect_uri: redirect_uri
+      }
+
+      response = connection.post do |req|
+        req.url "https://api.weibo.com/oauth2/access_token", body
+      end
+
+      MultiJson.decode(response.body)
+    end
+  end
+end
